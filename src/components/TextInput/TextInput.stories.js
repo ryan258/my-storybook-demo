dbee30de438eab1507d09b3fcfc150c8a40b26f2
Ryan Johnson <ryanleejwebdev@gmail.com>
2020-11-26 12:53:15 -0600

    Sort the preview items in the storybook app.

--- src/components/TextInput/TextInput.stories.js
+++ src/components/TextInput/TextInput.stories.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import TextInput from './TextInput';
+import React from "react"
+import TextInput from "./TextInput"
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
-  title: 'Form/TextInput',
-  component: TextInput,
-};
+  title: "Form/TextInput",
+  component: TextInput
+}
 
-export const Small = () => <TextInput size="small" placeholder="Small Size" />;
-export const Medium = () => (
-  <TextInput size="medium" placeholder="Medium Size" />
-);
-export const Large = () => <TextInput size="large" placeholder="Large Size" />;
+export const Small = () => <TextInput size="small" placeholder="Small Size" />
+export const Medium = () => <TextInput size="medium" placeholder="Medium Size" />
+export const Large = () => <TextInput size="large" placeholder="Large Size" />
+
+// Update the display name in the Storybook sidebar
+Small.storyName = "Small Input"
