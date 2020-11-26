5464440e3315800219b8925e2ac791fe70cbf41d
Ryan Johnson <ryanleejwebdev@gmail.com>
2020-11-26 13:23:46 -0600

    ESLint ignore export deefault in Subscription.stories.

--- src/components/Subscription/Subscription.stories.js
+++ src/components/Subscription/Subscription.stories.js
@@ -2,6 +2,7 @@ import React from "react"
 import { Primary } from "../MyButton/MyButton.stories"
 import { Large } from "../TextInput/TextInput.stories"
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default {
   title: "Form/Subscription"
 }
