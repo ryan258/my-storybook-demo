53f2c75a443bca1145f3028b9b6d8ba21264af3e
Ryan Johnson <ryanleejwebdev@gmail.com>
2020-11-26 12:54:27 -0600

    Sort the preview items in the storybook app.

--- src/components/MyButton/MyButton.stories.js
+++ src/components/MyButton/MyButton.stories.js
@@ -1,21 +1,19 @@
-import React from 'react';
-import MyButton from './MyButton';
+import React from "react"
+import MyButton from "./MyButton"
 
 // Write story in component-story format
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
   // the text you see in the left handle panel (required)
-  title: 'Form/MyButton',
+  title: "Form/Control/MyButton",
   // the component you are targeting with these stories
-  component: MyButton,
-};
+  component: MyButton
+}
 
 // specify the named exports, each one is a story, a sub menu item is created for each named export
 // each is basically a react component
 
-export const Primary = () => <MyButton variant="primary">Primary</MyButton>;
-export const Secondary = () => (
-  <MyButton variant="secondary">Secondary</MyButton>
-);
-export const Success = () => <MyButton variant="success">Success</MyButton>;
-export const Danger = () => <MyButton variant="danger">Danger</MyButton>;
+export const Primary = () => <MyButton variant="primary">Primary</MyButton>
+export const Secondary = () => <MyButton variant="secondary">Secondary</MyButton>
+export const Success = () => <MyButton variant="success">Success</MyButton>
+export const Danger = () => <MyButton variant="danger">Danger</MyButton>
