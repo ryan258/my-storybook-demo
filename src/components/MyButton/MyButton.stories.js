31cacc35e90cdefbdf70b33cb7243f964b6a91dc
Ryan Johnson <ryanleejwebdev@gmail.com>
2020-11-27 21:30:49 -0600

    Creating stories using an args approach.

--- src/components/MyButton/MyButton.stories.js
+++ src/components/MyButton/MyButton.stories.js
@@ -7,7 +7,10 @@ export default {
   // the text you see in the left handle panel (required)
   title: "Form/Control/MyButton",
   // the component you are targeting with these stories
-  component: MyButton
+  component: MyButton,
+  args: {
+    children: "Bizutton"
+  }
 }
 
 // specify the named exports, each one is a story, a sub menu item is created for each named export
@@ -17,3 +20,23 @@ export const Primary = () => <MyButton variant="primary">Primary</MyButton>
 export const Secondary = () => <MyButton variant="secondary">Secondary</MyButton>
 export const Success = () => <MyButton variant="success">Success</MyButton>
 export const Danger = () => <MyButton variant="danger">Danger</MyButton>
+
+const Template = args => <MyButton {...args} />
+
+export const PrimaryA = Template.bind({})
+PrimaryA.args = {
+  variant: "primary"
+  // children: "Primary Args"
+}
+
+export const LongPrimaryA = Template.bind({})
+LongPrimaryA.args = {
+  ...PrimaryA.args
+  // children: "Long Primary Args"
+}
+
+export const SecondaryA = Template.bind({})
+SecondaryA.args = {
+  variant: "secondary"
+  // children: "Secondary Args"
+}
